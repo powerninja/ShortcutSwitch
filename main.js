801e32473aedc58db76c5128fa1a34e379c205b3
powerninja <asdyuy23@gmail.com>
2023-07-08 00:33:11 +0900

    pythonでInkファイルを分析する(処理を作りたかった)

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const { app, Menu, Tray, BrowserWindow } = require('electron');
+const { PythonShell } = require('python-shell');
+const path = require('path');
 
 let tray = null; // ここでtrayを宣言
 
 app.whenReady().then(() => {
-  const path = require('path');
   tray = new Tray(path.join(__dirname, 'appのコピー.png')); // ここでtrayに値を代入
   const contextMenu = Menu.buildFromTemplate([
     { label: 'Item1', type: 'radio' },
@@ -15,8 +16,20 @@ app.whenReady().then(() => {
   tray.setContextMenu(contextMenu);
 });
 
-//アプリが❌ボタンで閉じられても終了しない
 app.on('window-all-closed', (event) => {
   // デフォルトの挙動を抑制する
   event.preventDefault();
 });
+
+function parseLnk(lnkFilePath) {
+  // Run the Python script and get the result via a Promise
+  return new Promise((resolve, reject) => {
+    PythonShell.run('parse_lnk.py', { args: [lnkFilePath] }, (err, results) => {
+      if (err) reject(err);
+      else resolve(results[0]); // Results is an array, we only need the first value
+    });
+  });
+}
+
+// Just for testing purposes
+parseLnk('/path/to/your/lnkfile.lnk').then(console.log).catch(console.error);
