ef424fa3c9dfeabe4406cbdc2492649a67b16d07
powerninja <asdyuy23@gmail.com>
2023-07-13 23:18:18 +0900

    不要なコード削除

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ let tray = null; // ここでtrayを宣言
 
 app.whenReady().then(() => {
   tray = new Tray(path.join(__dirname, 'appのコピー.png')); // ここでtrayに値を代入
+  //メニューバーのメニュー設定
   const contextMenu = Menu.buildFromTemplate([
     { label: 'Item1', type: 'radio' },
     { label: 'Item2', type: 'radio' },
@@ -29,15 +30,10 @@ app.whenReady().then(() => {
     // Convert the string to Shift_JIS
     const sjisString = iconv.decode(Buffer.from(binaryString, 'binary'), 'Shift_JIS');
 
-    // うまくいった
-    // const regex = /192\.168\.254\.6\\Company\\00_AA(?:[^\\]*\\)*/;
-
-    //バックスラッシュの後ろがnull文字とされているので、null文字が出てくるまでの文字列を取得する
-
     //AD2の場合
     // const regex = /192\.168\.254\.6\\Company.*?(?=\\\0|$)/;
 
-    //AD.linの場合
+    //AD.linの場合(両方可能？)
     const regex = /\\192\.168\.254\.6\\Company.*?(?=\\\0|$)/;
 
     // 192.168.254.6~バックスラッシュnull文字まで抽出する
@@ -48,31 +44,5 @@ app.whenReady().then(() => {
     } else {
       console.log('No match found');
     }
-
-    // if (matchResult) {
-    //   const cutPosition = matchResult.indexOf('x');
-
-    //   if (cutPosition !== -1) {
-    //     const newString = matchResult.substring(0, cutPosition + 41); // +2 to include the two backslashes
-    //     console.log(newString, 'newString');
-    //     let rrr = '';
-    //     for (let i = 0; i < matchResult.length; i++) {
-    //       if (matchResult.charAt(i) === '\0') {
-    //         break;
-    //       } else {
-    //         rrr += matchResult.charAt(i);
-    //       }
-    //     }
-    //   } else {
-    //     console.log('No double backslashes found');
-    //   }
-    // } else {
-    //   console.log('No match found');
-    // }
-  });
-
-  app.on('window-all-closed', (event) => {
-    // デフォルトの挙動を抑制する
-    event.preventDefault();
   });
 });
